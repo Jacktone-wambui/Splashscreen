e1f81cd463950437bef1d8bdc9583dc34dce488a
jacktone-wambui <jacktoneabongo@gmail.com>
2022-05-21 08:10:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static  int SPLASH_SCREEN = 5000;
+
+    //variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo,slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+         //Animations
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        image=findViewById(androidx.appcompat.R.id.image);
+        logo=findViewById(R.id.textView);
+        slogan=findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+         new Handler().postDelayed(new Runnable() {
+             @Override
+             public void run() {
+                 Intent intent = new Intent(MainActivity.this,WarrenStore.class);
+                 startActivity(intent);
+                 finish();
+             }
+         },SPLASH_SCREEN);
+    }
+}
